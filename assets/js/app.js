6456d57beb612d98b004cb60ae9c96f0a5ed5326
gypsyelliot <elliot@elliotreed.net>
2018-02-02 13:42:31 -0700

    added links

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCvpTUzbU9prXZLDP9BtwBE1viuztTbt0k",
+    authDomain: "audiosauce-6eb52.firebaseapp.com",
+    databaseURL: "https://audiosauce-6eb52.firebaseio.com",
+    projectId: "audiosauce-6eb52",
+    storageBucket: "",
+    messagingSenderId: "266026610182"
+  };
+  firebase.initializeApp(config);
